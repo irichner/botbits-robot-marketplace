d59ca826f6fdf9a321eac3fb6d77451bc8b45e4d
Israel Richner <israel.richner@gamil.com>
2025-12-28 23:43:00 -0600

    Initial commit: Set up project structure and basic configurations

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f703b300961a6ec0"; }
+/******/ 		__webpack_require__.h = function() { return "a4abfde28546095c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
